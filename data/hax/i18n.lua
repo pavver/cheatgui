ee757a237801d4c637ebc5f323e304dc8e2d7611
pavver <pavvers1@gmail.com>
2026-06-27 01:31:32 +0300

    Fix game locale detection

--- data/hax/i18n.lua
+++ data/hax/i18n.lua
@@ -1,28 +1,41 @@
 local locale_aliases = {
   en = "en",
+  English = "en",
   br = "pt-br",
   cn = "zh-cn",
   de = "de",
+  Deutsch = "de",
   es = "es-es",
   ["es-es"] = "es-es",
+  ["Español"] = "es-es",
   fr = "fr-fr",
   ["fr-fr"] = "fr-fr",
+  ["Français"] = "fr-fr",
   it = "it",
+  Italiano = "it",
   ja = "jp",
   jp = "jp",
+  ["日本語"] = "jp",
   ko = "ko",
+  ["한국어"] = "ko",
   pl = "pl",
+  Polska = "pl",
   pt = "pt-br",
   ["pt-br"] = "pt-br",
+  ["Português (Brasil)"] = "pt-br",
   ru = "ru",
+  ["русский"] = "ru",
   ua = "uk",
   uk = "uk",
+  ["Українська"] = "uk",
+  ["українська"] = "uk",
   zh = "zh-cn",
-  ["zh-cn"] = "zh-cn"
+  ["zh-cn"] = "zh-cn",
+  ["简体中文"] = "zh-cn"
 }
 
 local fallback_locale = "en"
-local detected_locale = GameTextGet("x")
+local detected_locale = GameTextGet("$current_language")
 local current_locale = locale_aliases[detected_locale] or fallback_locale
 local locale_path = "data/hax/i18n/"
 
